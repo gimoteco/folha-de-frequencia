687a008416c0b9c0d4145925fad47fe7db1448d4
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2018-04-26 11:32:35 -0400

    Separação das regras de negócio

---
+++ core.py
@@ -0,0 +1,42 @@
+from datetime import timedelta
+from datetime import datetime
+import random
+
+class GeradorDePonto:
+
+    def __init__(self, horario_de_chegada_oficial, carga_horaria, preencher_fim_de_semana, minimo_de_minutos_de_almoco, variacao_maxima, minutos_de_almoco):
+        self.carga_horaria = carga_horaria
+        self.horario_de_chegada_oficial = horario_de_chegada_oficial
+        self.preencher_fim_de_semana = preencher_fim_de_semana
+        self.minimo_de_minutos_de_almoco = minimo_de_minutos_de_almoco
+        self.minutos_de_almoco = minutos_de_almoco
+        self.variacao_maxima = variacao_maxima
+
+    def gerar_por_periodo(self, inicio, fim):
+        dia = inicio
+        um_dia = timedelta(days=1)
+        while dia <= fim:
+            data_da_chegada_oficial = datetime(dia.year, dia.month, dia.day, self.horario_de_chegada_oficial.hour, self.horario_de_chegada_oficial.minute)
+            anotacoes_do_dia = self.obter_anotacoes(data_da_chegada_oficial)
+            registros_do_dia = (dia, )
+            eh_dia_de_semana = dia.weekday() < 5
+            if eh_dia_de_semana or self.preencher_fim_de_semana:
+                registros_do_dia = registros_do_dia + anotacoes_do_dia
+            dia += um_dia
+            yield registros_do_dia
+
+    def obter_anotacoes(self, data_da_chegada_oficial):
+        variacao_de_permanencia = self.obter_variacao_aleatoria_de_tempo()
+        variacao_da_chegada = self.obter_variacao_aleatoria_de_tempo()
+        duracao_do_almoco = timedelta(minutes=random.randint(self.minimo_de_minutos_de_almoco, self.minutos_de_almoco))
+        metade_do_expediente = timedelta(hours=self.carga_horaria / 2)
+        permanencia_da_manha = metade_do_expediente  + variacao_de_permanencia
+        horario_de_chegada = data_da_chegada_oficial + variacao_da_chegada
+        saida_da_manha = horario_de_chegada + permanencia_da_manha
+        chegada_da_tarde = saida_da_manha + duracao_do_almoco
+        saida_da_tarde = chegada_da_tarde - variacao_de_permanencia + metade_do_expediente
+        total = saida_da_manha - horario_de_chegada + saida_da_tarde - chegada_da_tarde
+        return (horario_de_chegada, saida_da_manha, chegada_da_tarde, saida_da_tarde, total)
+
+    def obter_variacao_aleatoria_de_tempo(self):
+        return timedelta(minutes=random.randint(0, self.variacao_maxima)) * random.choice([-1, 1])
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from prettytable import PrettyTable
-from datetime import datetime
+from datetime import datetime, time
 from datetime import timedelta
 import random
 import argparse
 import csv
+from core import GeradorDePonto
 
 hoje = datetime.now()
 parser = argparse.ArgumentParser(description='Gera uma folha de ponto aleatória')
@@ -23,46 +24,34 @@ INICIO_DO_CALENDARIO = datetime.strptime(args.inicio, '%d/%m/%y')
 FIM_DO_CALENDARIO = datetime.strptime(args.fim, '%d/%m/%y')
 HORA_DA_CHEGADA, MINUTO_DA_CHEGADA = map(lambda parte: int(parte), args.hora_de_chegada.split(':'))
 DIAS_DA_SEMANA = {5: 'SÁBADO', 6: 'DOMINGO'}
-
-def obter_anotacoes(chegada_oficial):
-    variacao_de_permanencia = timedelta(minutes=random.randint(0, args.variacao_maxima)) * random.choice([-1, 1])
-    variacao_da_chegada = timedelta(minutes=random.randint(0, args.variacao_maxima)) * random.choice([-1, 1])
-    duracao_do_almoco = timedelta(minutes=random.randint(args.minimo_de_almoco, args.minutos_de_almoco))
-    metade_do_expediente = timedelta(hours=args.carga_horaria / 2)
-    permanencia_da_manha = metade_do_expediente  + variacao_de_permanencia
-    horario_de_chegada = chegada_oficial + variacao_da_chegada
-    saida_da_manha = horario_de_chegada + permanencia_da_manha
-    chegada_da_tarde = saida_da_manha + duracao_do_almoco
-    saida_da_tarde = chegada_da_tarde - variacao_de_permanencia + metade_do_expediente
-    total = saida_da_manha - horario_de_chegada + saida_da_tarde - chegada_da_tarde
-    return (horario_de_chegada, saida_da_manha, chegada_da_tarde, saida_da_tarde, total)
+tempo_da_chegado = time(hour=HORA_DA_CHEGADA, minute=MINUTO_DA_CHEGADA)
+gerador = GeradorDePonto(tempo_da_chegado, args.carga_horaria, args.preencher_fim_de_semana, args.minimo_de_almoco, args.variacao_maxima, args.minutos_de_almoco)
+registros = gerador.gerar_por_periodo(INICIO_DO_CALENDARIO, FIM_DO_CALENDARIO)
 
 def formatar_hora(data):
     return data.strftime('%H:%M')
 
-cabecalho = ["Data", "Entrada matutino", "Saída matutino", "Entrada vespertino", "Saída vespertino", "Horas extras", "Assinatura"]
+cabecalho = ["Data", "Entrada matutino", "Saída matutino", "Entrada vespertino", "Saída vespertino"]
 tabela = PrettyTable(cabecalho)
 tabela.padding_width = 1
-dia = INICIO_DO_CALENDARIO
-um_dia = timedelta(days=1)
 
 with open(args.arquivo_de_saida, 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile)
-    spamwriter.writerow(cabecalho)    
-    while dia <= FIM_DO_CALENDARIO:
+    spamwriter.writerow(cabecalho)
+    for registro in registros:
+        dia = registro[0]
         dia_formatado = dia.strftime('%d/%m')
-        anotacoes_do_dia = obter_anotacoes(datetime(dia.year, dia.month, dia.day, HORA_DA_CHEGADA, MINUTO_DA_CHEGADA))
+        anotacoes_do_dia = registro
         linha = []
         if dia.weekday() < 5 or args.preencher_fim_de_semana:
-            linha = [dia_formatado, formatar_hora(anotacoes_do_dia[0]), formatar_hora(anotacoes_do_dia[1]), formatar_hora(anotacoes_do_dia[2]), formatar_hora(anotacoes_do_dia[3]), "" , ""]
+            linha = [dia_formatado, formatar_hora(anotacoes_do_dia[1]), formatar_hora(anotacoes_do_dia[2]), formatar_hora(anotacoes_do_dia[3]), formatar_hora(anotacoes_do_dia[4])]
             tabela.add_row(linha)
             spamwriter.writerow(linha)
         else:
             dia_da_semana = DIAS_DA_SEMANA[dia.weekday()]
-            linha = [dia_formatado] + [dia_da_semana] * 6
+            linha = [dia_formatado] + [dia_da_semana] * 4
             tabela.add_row(linha)
             spamwriter.writerow(linha)
-        dia += um_dia    
 
 linhas_da_tabela = tabela.get_string()
 print(linhas_da_tabela)
